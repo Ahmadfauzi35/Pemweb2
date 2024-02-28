053a7e9948933ce392f30ab27e5a1d7ebb03de92
Ahmadfauzi35 <146049749+Ahmadfauzi35@users.noreply.github.com>
2024-02-28 17:03:13 +0700

    Delete array.php

--- array.php
+++
@@ -1,8 +0,0 @@
-<?php
-// arrray indexing
-$mhs1 = ['0110123196', 'Fauzi', 'Sistem Informasi', [3.5, 3.7, 4.0]];
-
-// panggil array 
-echo 'Nama: ' . $mhs1[1] . '<br>'; // konkatenasi string
-echo "NIM: $mhs1[0] <br>"; // string interpolation
-echo "IPS semester 1: " . $mhs1[3][0];
